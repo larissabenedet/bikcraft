7bc6e27ed24e7c023875e6d0038dc8015c1143ab
larissabenedet <larissavb2014@outlook.com>
2021-05-22 17:04:32 -0300

    Ajustes finais no responsivo e adição de plugins

---
+++ js/script.js
@@ -0,0 +1,11 @@
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote",
+        time: 5000
+    })
+    new SimpleSlide({
+        slide: "portfolio",
+        time: 5000,
+        nav: true
+    })
+}
