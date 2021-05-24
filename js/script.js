5f477be769f407e55962176c36e4f571e19fa95c
larissabenedet <larissavb2014@outlook.com>
2021-05-24 13:10:54 -0300

    SEO e envio de formulário

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
     new SimpleAnime();
 }
+
+if (window.SimpleForm) {
+    new SimpleForm({
+        form: ".formphp",
+        button: "#enviar",
+        erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o e-mail contato@bikcraft.com.</p></div>",
+        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso!</h2><p>Em breve entro em contato com você.</p></div>"
+    });
+}
